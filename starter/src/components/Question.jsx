600f7d51cc4b1f67d5f95980af7fd73761cfb37d
Emile0001 <botesEH@gmail.com>
2024-10-07 15:49:13 +0200

    show and hide info logic

--- starter/src/components/Question.jsx
+++ starter/src/components/Question.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 export const Question = ({ title, info }) => {
+    const [showInfo, setShowInfo] = useState(false);
     return (
         <article className="question">
             <header>
                 <h4>{title}</h4>
+                <button
+                    className="question-btn"
+                    onClick={() => {
+                        setShowInfo(!showInfo);
+                    }}
+                >
+                    {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+                </button>
             </header>
 
-            <p>{info}</p>
+            {showInfo && <p>{info}</p>}
         </article>
     );
 };
